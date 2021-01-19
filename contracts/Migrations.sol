8b0e45840777e9107d00d499778b64858f64c868
sam bacha <sam@freighttrust.com>
2021-01-18 23:57:56 -0800

    fix(deploy): update migrations

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,15 +1,25 @@
-pragma solidity >=0.4.22 <0.8.0;
+// SPDX-License-Identifier: Apache-2.0
+
+pragma solidity >=0.5.0 <0.7.0;
 
 contract Migrations {
-    address public owner = msg.sender;
-    uint256 public last_completed_migration;
+  address public owner;
+  uint public last_completed_migration;
 
-    modifier restricted() {
-        require(msg.sender == owner, "This function is restricted to the contract's owner");
-        _;
-    }
+  modifier restricted() {
+    if (msg.sender == owner) _;
+  }
 
-    function setCompleted(uint256 completed) public restricted {
-        last_completed_migration = completed;
-    }
+  constructor() public {
+    owner = msg.sender;
+  }
+
+  function setCompleted(uint completed) public restricted {
+    last_completed_migration = completed;
+  }
+
+  function upgrade(address new_address) public restricted {
+    Migrations upgraded = Migrations(new_address);
+    upgraded.setCompleted(last_completed_migration);
+  }
 }
